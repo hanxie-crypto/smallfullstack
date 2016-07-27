34166e7dd23edc7df97674f2b2f7fd15ec023a36
wq123456 <aitongbian123@163.com>
2016-07-27 17:53:14 +0800

    all

--- app.js
+++ app.js
@@ -57,4 +57,10 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+app.set('port', 3001);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+
+
